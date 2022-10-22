368359395e3c2eb711c532a74c455cb087f5b6af
gcarolina <karolinaganizoda@gmail.com>
2022-10-22 15:28:27 +0300

    Project has been created

---
+++ UIKitElementsApp/UIKitElementsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitElementsApp
+//
+//  Created by Carolina on 22.10.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
